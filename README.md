a1a1db6dc31ff6181808720f0a32497a72d784dc
Alexander Shagin <saaivs@gmail.com>
2022-04-12 15:45:57 +0300

    refactor: ssl-info initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ssl-info
-CLI SSL checker
+
+CLI wrapper over [Node SSL Checker](https://github.com/dyaa/ssl-checker).
+
+## Features
+
+* Print to console JSON response or specific value (daysRemaining, valid, validFrom, validTo, validFor).
+
+## Examples
+
+```bash
+ssl-info --help
+ssl-info google.com
+ssl-info google.com daysRemaining
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+const sslChecker = require("ssl-checker")
+const path = require("path");
+const pckg = require(path.join(__dirname, "package.json"))
+
+const app = Object.keys(pckg.bin)[0]
+
+// https://www.npmjs.com/package/yargs
+const { argv } = require("yargs")
+  .option("hostname", {
+    alias: "h",
+    type: "string",
+    description: "Hostname",
+  })
+  .option("property", {
+    alias: "p",
+    type: "string",
+    description:
+      "Property (daysRemaining, valid, validFrom, validTo, validFor)",
+  })
+  .usage(`Usage: ${app} `)
+  .epilog(
+    ["https://art-ws.com", pckg.description ?? "", "Copyright 2022"].join(", ")
+  )
+  .example(`${app} google.com daysRemaining`, "")
+
+async function main(argv) {
+  const hostname = argv.hostname || argv._[0]
+  const prop = argv.hostname || argv._[1]
+  const info = await sslChecker(hostname)
+  if (prop) {
+    console.log(info[prop])
+  } else {
+    console.log(JSON.stringify(info))
+  }
+}
+
+main(argv).catch((e) => {
+  console.error(e)
+  process.exit(1)
+})
